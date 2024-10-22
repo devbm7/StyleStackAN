e129c699be6aa2f02b465a507b7fe94fc87d5d54
Dev Makwana <devmakwana7704@gmail.com>
2024-10-22 09:52:40 +0530

    Modification v1.04 - cleaning up commented code for Clothes.java

--- app/src/main/java/com/example/initial/Clothes.java
+++ app/src/main/java/com/example/initial/Clothes.java
@@ -1,49 +1,5 @@
 package com.example.initial;
 
-//public class Clothes {
-//    private String id; // Document ID
-//    private String name;
-//    private String category;
-//    private String imageUrl;
-//    private boolean inLaundry;
-//
-//    public Clothes() {
-//        // Default constructor required for Firestore deserialization
-//    }
-//
-//    public Clothes(String id, String name, String category, String imageUrl, boolean inLaundry) {
-//        this.id = id;
-//        this.name = name;
-//        this.category = category;
-//        this.imageUrl = imageUrl;
-//        this.inLaundry = inLaundry;
-//    }
-//
-//    public String getId() {
-//        return id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public String getCategory() {
-//        return category;
-//    }
-//
-//    public String getImageUrl() {
-//        return imageUrl;
-//    }
-//
-//    public boolean isInLaundry() {
-//        return inLaundry;
-//    }
-//
-//    public void setInLaundry(boolean inLaundry) {
-//        this.inLaundry = inLaundry;
-//    }
-//}
-
 public class Clothes {
     private String id;
     private String name;
